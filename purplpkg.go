a9959f62a02e023bf2cdae9e8d48d5f165383337
froggitti <85637569+froggitti@users.noreply.github.com>
2026-01-14 17:14:31 -0500

    Import things needed for OS execution and strings

--- purplpkg.go
+++ purplpkg.go
@@ -1,11 +1,13 @@
 package main
 
 import "fmt"
+import "strings"
+import "os"
+import "os/exec"
 
 func main() {
   mirrorMain := "https://www.froggitti.net/vector-mirror/"
   mirrorSecondary := "https://net-3.froggitti.net/vector-mirror/"
   fmt.Println(mirrorMain)
   fmt.Println(mirrorSecondary)
-
 }
