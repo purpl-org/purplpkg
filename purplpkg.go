09650dc9a71789fe7bb85f2d0ecc7b52bdeaf4b7
froggitti <85637569+froggitti@users.noreply.github.com>
2026-01-14 17:21:43 -0500

    Add empty argument check

--- purplpkg.go
+++ purplpkg.go
@@ -10,4 +10,14 @@ func main() {
   mirrorSecondary := "https://net-3.froggitti.net/vector-mirror/"
   fmt.Println(mirrorMain)
   fmt.Println(mirrorSecondary)
+
+if len(os.Args) < 2 {
+    fmt.Println("purplpkg by purpl")
+    fmt.Println("-------------------")
+    fmt.Println("Usage:")
+    fmt.Println("install: Installs a package")
+    fmt.Println("package-list: Lists currently available packages")
+    os.Exit(1)
+}
+  
 }
