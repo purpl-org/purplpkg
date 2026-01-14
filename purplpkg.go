afdca58f4498ab15e1d17a91c55120aea19c07a5
froggitti <85637569+froggitti@users.noreply.github.com>
2026-01-14 17:10:33 -0500

    Add secondary mirror to variable list

--- purplpkg.go
+++ purplpkg.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
   mirrorMain := "https://www.froggitti.net/vector-mirror/"
+  mirrorSecondary := "https://net-3.froggitti.net/vector-mirror/"
   fmt.Println(mirrorMain)
+  fmt.Println(mirrorSecondary)
 
 }
