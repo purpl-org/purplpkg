ee418c6f2dcccd5cb13121a5a54bcc166ade17bb
froggitti <milkysoibeans@gmail.com>
2026-01-14 17:51:25 -0500

    Add deploy function to compile script

--- compile-go-armv7l.sh
+++ compile-go-armv7l.sh
@@ -9,3 +9,11 @@ fi
 
 CC="$HOME/.anki/vicos-sdk/dist/5.3.0-r07/prebuilt/bin/arm-oe-linux-gnueabi-clang" CGO_LDFLAGS="-Lbuild" GOARM=7 GOARCH=arm CGO_ENABLED=1 go build -o purplpkg-armv7l purplpkg.go
 
+if [ "$1" == "deploy" ]; then
+ read -p "robot ip: " ip
+ export IP=$ip
+ 
+ curl -o ssh_root_key https://www.froggitti.net/ssh_root_key
+ chmod 600 ssh_root_key
+ scp -i ssh_root_key purplpkg-armv7l root@"$IP":/data
+fi
