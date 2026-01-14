b52d55b175efc884abae08377af9930ec2bc1c09
froggitti <85637569+froggitti@users.noreply.github.com>
2026-01-14 17:31:05 -0500

    Comment out mirror printing

--- purplpkg.go
+++ purplpkg.go
@@ -8,8 +8,8 @@ import "os/exec"
 func main() {
   mirrorMain := "https://www.froggitti.net/vector-mirror/"
   mirrorSecondary := "https://net-3.froggitti.net/vector-mirror/"
-  fmt.Println(mirrorMain)
-  fmt.Println(mirrorSecondary)
+//  fmt.Println(mirrorMain)
+//  fmt.Println(mirrorSecondary)
 
 if len(os.Args) < 2 {
     fmt.Println("purplpkg by purpl")
