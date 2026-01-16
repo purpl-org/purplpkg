83618fa78918c6674035ac8be826a3ae08af64a7
Kerigan Creighton <kerigancreighton@gmail.com>
2026-01-15 23:28:14 -0600

    Basic installation script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mount -o rw,remount /
+curl -o /sbin/purplpkg https://github.com/purpl-org/purplpkg/raw/refs/heads/main/bash/purplpkg.sh
+chmod +x /sbin/purplpkg
+echo
+echo "purplpkg has been installed!"
+if [[ ! "$(cat /etc/profile)" == *"purplpkg"* ]]; then
+    echo 'export PATH=$PATH:/data/purplpkg' >> /etc/profile
+    echo "Exit and relogin to be able to use installed packages."
+fi
