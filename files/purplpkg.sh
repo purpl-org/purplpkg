06d3a7f9787ee3592bff6996ef631c0f7b07a159
froggitti <85637569+froggitti@users.noreply.github.com>
2026-01-15 20:05:09 -0500

    Make some curl commands silent, remove INSTALL_DIR variable, and add 404 checking

--- files/purplpkg.sh
+++ files/purplpkg.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env bash
 
-if [ ! -d "$INSTALL_DIR" ]; then
- mkdir -p "$INSTALL_DIR"
-fi
-
-if [ ! -d "$INSTALL_DIR"/versions ]; then
- mkdir -p "$INSTALL_DIR"/versions
-fi
-
-export PATH=""$INSTALL_DIR":$PATH"
-
-set -e
-#mount -o rw,remount /
-#rm -rf "$INSTALL_DIR"/*
-cd "$INSTALL_DIR"
-
 BASE_URL="https://www.froggitti.net/vector-mirror/"
-#https://purplpkg.net-3.froggitti.net/ will be a backup/secondary mirror - still need to set it up
 BASE_URL_2="https://purplpkg.net-3.froggitti.net/"
 
 MIRROR_URL="https://www.froggitti.net/vector-mirror/"
 
-if [ "$1" == "package-list" ]; then
- curl https://www.froggitti.net/vector-mirror/package.list
- exit 0
+if [ ! -d /data/purplpkg ]; then
+ mkdir -p /data/purplpkg
 fi
 
-INSTALL_DIR="/data/purplpkg"
+if [ ! -d /data/purplpkg/versions ]; then
+ mkdir -p /data/purplpkg/versions
+fi
+
+#export PATH="$INSTALL_DIR:$PATH"
+
+set -e
+
+cd /data/purplpkg
 
 if [ "$1" == "" ]; then
  echo purplpkg by purpl
@@ -34,6 +25,23 @@ if [ "$1" == "" ]; then
  echo Usage:
  echo install: Installs a package
  echo package-list: Lists currently available packages
+ echo mirror-list: Lists currently available mirrors
+ exit 0
+fi
+
+if [ "$1" != "package-list" ] && [ "$1" != "install" ] && [ "$1" != "mirror-list" ]; then
+ echo Unknown action "$1"
+ exit 1
+fi
+
+if [ "$1" == "package-list" ]; then
+ curl https://www.froggitti.net/vector-mirror/package.list
+ exit 0
+fi
+
+if [ "$1" == "mirror-list" ]; then
+ echo "$BASE_URL"
+ echo "$BASE_URL_2"
  exit 0
 fi
 
@@ -58,29 +66,34 @@ if [ "$2" == "" ]; then
  exit 1
 fi
 
-echo $PWD
-
-if [ ! "$PWD" == "$INSTALL_DIR" ]; then
+if [ ! "$PWD" == /data/purplpkg ]; then
  echo "We are in the wrong directory. Exiting..."
  exit 1
 fi
 
 if [[ "$2" == anki-* ]]; then
- rm "$INSTALL_DIR"/*.tar
+ rm /data/purplpkg/*.tar
 fi
 
 # Raise CPU frequency for faster downloads/installs
 echo 1267200 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
-export VERSION=$(curl https://www.froggitti.net/vector-mirror/"$2".version)
+export VERSION=$(curl --silent "$MIRROR_URL"/"$2".version)
 
 echo Downloading package "$2" from "$MIRROR_URL" with version "$VERSION"
-curl -o "$INSTALL_DIR"/"$2".tar.gz "$MIRROR_URL"/"$2".tar.gz
-curl -o "$INSTALL_DIR"/versions/"$2" "$MIRROR_URL"/"$2".version
+curl -o /data/purplpkg/"$2".tar.gz "$MIRROR_URL"/"$2".tar.gz
+curl --silent -o /data/purplpkg/versions/"$2" "$MIRROR_URL"/"$2".version
+
+if grep -q "<head><title>404 Not Found</title></head>" "$2".tar.gz; then
+ echo "Package is a 404. Deleting."
+ rm "$2".tar.gz
+ echo "Check the package name and try again."
+ exit 1
+fi
 
 if [[ ! "$2" == anki-* ]]; then
  echo "Installing..."
- gunzip "$INSTALL_DIR"/"$2".tar.gz
+ gunzip /data/purplpkg/"$2".tar.gz
  tar -xf "$2".tar
  echo "Cleaning up..."
  rm -rf "$2".tar
@@ -93,11 +106,11 @@ if [[ "$2" == anki-* ]]; then
  echo "Stop robot"
  systemctl stop anki-robot.target
  echo "Decompress anki folder"
- gunzip "$INSTALL_DIR"/"$2".tar.gz
- tar -xf "$INSTALL_DIR"/"$2".tar
+ gunzip /data/purplpkg/"$2".tar.gz
+ tar -xf /data/purplpkg/"$2".tar
  rm -rf /anki
  echo "Install /anki folder"
- mv "$INSTALL_DIR"/anki /anki
+ mv /data/purplpkg/anki /anki
  echo "Done"
  systemctl start anki-robot.target
 fi
